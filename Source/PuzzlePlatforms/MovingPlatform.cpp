9e8ec5d509acee23703fbb7322196837bf46f116
liyindong <2397619613@qq.com>
2021-06-12 00:17:47 +0800

    Add a moving platform
    Signed-off-by: liyindong <2397619613@qq.com>

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+
+	Speed = 10.f;
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority()) //判断是否为服务器
+	{
+		FVector Location = GetActorLocation();
+		Location += FVector(Speed * DeltaTime, 0, 0);
+		SetActorLocation(Location);
+	}
+
+}
